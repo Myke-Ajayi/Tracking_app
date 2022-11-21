697142de332dc81a1b1e98d76b2786df8c7fb7fe
Michael Ajayi <mike.endowed@icloud.com>
2022-11-20 23:17:44 -0500

    framework

---
+++ tracking/apps/userprofile/models.py
@@ -0,0 +1,11 @@
+
+
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Userprofile(models.Model):
+    user = models.ForeignKey(User, related_name='userprofie', on_delete=models.CASCADE)
+    active_team_id = models.IntegerField(default=0)
