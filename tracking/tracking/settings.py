26c5e44cace53fefaa142097d8cb5eb77b43c25a
Michael Ajayi <king_myke@michaels-air.myfiosgateway.com>
2022-12-01 21:40:14 -0500

    updated work

--- tracking/tracking/settings.py
+++ tracking/tracking/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_URL = 'login'
+LOGIN_URL_REDIRECT = 'myaccount'
+LOGOUT_URL = 'frontpage'
 # Application definition
 
 INSTALLED_APPS = [
--- tracking/tracking/urls.py
+++ tracking/tracking/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 
 from apps.core.views import frontpage, privacy, terms, plans
-from apps.userprofile.views import signup
+from apps.userprofile.views import signup, myaccount, edit_profile
 
 
 urlpatterns = [
@@ -33,7 +34,10 @@ urlpatterns = [
     # Auth
 
     path('signup/', signup, name='signup'),
-
+    path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('myaccount/', myaccount, name='myaccount'),
+    path('myaccount/edit_profile/', edit_profile, name='edit_profile'),
 ]
 
 
